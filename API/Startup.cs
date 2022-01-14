1d41ed2fd3b4ff6c7632dcced3412044679943a5
Kęstutis Keršis <kestu.kersis@gmail.com>
2022-01-14 11:57:10 +0200

    Removed swagger

--- API/Startup.cs
+++ API/Startup.cs
@@ -68,8 +68,6 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
             app.UseHttpsRedirection();
